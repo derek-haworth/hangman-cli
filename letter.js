0b413b2d1d6fcd303788f6d9df19c094b05069bf
derek-haworth <derekhaw1@gmail.com>
2018-02-12 12:37:56 -0600

    refine letter logic to account for dashed words

--- letter.js
+++ letter.js
@@ -1,16 +1,22 @@
-//constructor function that displays "_" or user guessed character based on what the user inputs.
+//constructor function that displays "_", "-" depending on word or user guessed character based on what the user inputs.
 
 var Letter = function(char) {
 	this.char = char;
 	this.guessed = false;
-	this.display = '_';
 	this.checkLetter = function(guess){
-	  if (guess.toLowerCase() === this.char.toLowerCase()){
-	    this.display = this.char;
-	    this.guessed = true;
-	  }
-	  return this.guessed;
+		if (this.char === " ") {
+			this.guessed = true;
+			return "  ";
+		} else if (this.char === "-") {
+			this.guessed = true;
+			return "- ";
+		} else if (this.guessed) {
+			return this.char + " ";
+		} else {
+			return "_ ";
+		}
 	}
+	  
 };
 
 module.exports = Letter;
