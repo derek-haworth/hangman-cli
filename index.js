e8d28e352cbe6ed72aba6159d7b1c3e18b05795f
Derek Haworth <derek_haworth@bradycorp.com>
2018-02-13 15:33:44 -0600

    build 'then' logic from guessAndCheck prompt

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var hangGame = {
 	lives: 0,
 	currentWord: "",
 	alphabetArr: [],
+	wrongGuess: [],
 
 	// Creates alphabet array and pushes into empty array defined in hangGame object
 	alphabet: function () {
@@ -19,36 +20,37 @@ var hangGame = {
 	},
 
 	init: function() {
-		hangGame.alphabet();
 		var randomWord = Math.floor(Math.random() * this.wordArr.length);
 		var chosenWord = this.wordArr[randomWord].toUpperCase();
-		hangGame.lives = 10;
-		hangGame.currentWord = new Word(chosenWord);
+		this.lives = 10;
+		this.currentWord = new Word(chosenWord);
 
 		// Build Intro
 
 		// get method from letter.js
 		hangGame.currentWord.renderWord();
-		hangGame.askQuestion();
+		//the magic method that prompts to Type in a letter
+		this.guessAndCheck();
 	},
 
 	newGame: function() {
 		inquirer
-			.prompt([
-				{
-		      type: "confirm",
-		      message: "Do you want to play again?",
-		      name: "confirm",
-		      default: true
+		.prompt([
+			{
+		    	type: "confirm",
+		    	message: "Do you want to play again?",
+		    	name: "confirm",
+		    	default: true
 		    }
-		  ])
-		  .then(function(answer) {
+		])
+		.then(function(answer) {
+
 		   	if (answer.confirm) {
 		   		hangGame.init();
 		   	} else {
 		   		hangGame.endGame();
 		   	}
-		  });
+		});
 	},
 
 	endGame: function() {
@@ -56,28 +58,61 @@ var hangGame = {
 		return;
 	},
 
-	askQuestion: function() {
+	guessAndCheck: function() {
 		inquirer
-			.prompt([
-				{
-			    type: "input",
-			    message: "Type in a letter and hit 'Enter':",
-			    name: "guess",
-			    validate: function(input) {
-			    	var done = this.async();
-			    	var key = input.toUpperCase();
+		.prompt([
+			{
+		    	type: "input",
+		    	message: "Type in a letter and hit 'Enter':",
+		    	name: "guess",
+		    	validate: function(input) {
+
+		    		var done = this.async();
+		    		var key = input.toUpperCase();
+
 				  	if (!hangGame.alphabetArr.includes(key) ) {
 				  		done("Please enter a letter.");
 				  		return;
 				  	}
 				  	done(null, true);
 				  }
-				}
+			}
 	    ])
 	    .then(function(answer) {
-	    	console.log(answer);
+	    	var guess = answer.guess.toUpperCase();
+	    	console.log("You guessed: ", guess);
+
+	    	var found = hangGame.currentWord.checkLetter(guess);
+	    	hangGame.currentWord.renderWord();
+
+	    	if (!found) {
+
+	    		hangGame.lives--;
+
+	    		if (hangGame.lives === 0) {
+	    			console.log("\nSorry, the answer was '" + hangGame.currentWord.value + "'\n");
+	    			hangGame.newGame();
+	    		} else {
+	    			console.log("Try again!");
+	    			console.log("Status report: lives = " + hangGame.lives);
+	    			hangGame.guessAndCheck();
+	    		}	    		
+	    	} else {
+
+	    		console.log("Clever girl!");
+	    		hangGame.currentWord.isWordFound();
+
+	    		if (hangGame.currentWord.found) {
+	    			console.log("\nYou won!\n");
+	    			hangGame.newGame();
+	    		} else {
+	    			hangGame.guessAndCheck();
+	    		}
+	    	}	    	
 	    });
 	}
 }
 
+// create the letters for the alphabet
+hangGame.alphabet();
 hangGame.init();
